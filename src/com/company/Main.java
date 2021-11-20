3a1df0238f1d89af3338b0b9c570e72e8a0320ee
Panda <myxamopking@gmail.com>
2021-11-20 23:21:16 +0300

    Домашняя работа 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,23 +16,23 @@ public class Main {
     System.out.println("Символы: " + g);
     System.out.println("Логические: " + h);
 
-    float boxerNomderOneWeight = 78.2f;
-    float boxerNomberTwoWeight = 82.7f;
-    float totalWeight = boxerNomderOneWeight + boxerNomberTwoWeight;
+    float boxerNumberOneWeight = 78.2f;
+    float boxerNumberTwoWeight = 82.7f;
+    float totalWeight = boxerNumberOneWeight + boxerNumberTwoWeight;
     System.out.println("Общий вес бойцов: " + totalWeight + " кг");
-    float weightDifference = boxerNomberTwoWeight - boxerNomderOneWeight;
+    float weightDifference = boxerNumberTwoWeight - boxerNumberOneWeight;
     System.out.println("Разница в весе бойцов: " + weightDifference + " кг");
 
     short banana = 5;
-    short bananaWeightGm = 80;
+    short bananaWeight = 80;
     short milk100Ml = 2;
-    short milkWeightGMOf100Ml = 105;
+    short milk100MlWeight = 105;
     short iceCream = 2;
-    short iceCreamWeightGM = 100;
+    short iceCreamWeight = 100;
     short rawEgg = 4;
-    short rawEggWeightGM = 70;
-    int breakfastWeightGm = (banana * bananaWeightGm) + (milk100Ml * milkWeightGMOf100Ml) + (iceCream * iceCreamWeightGM)
-            + (rawEgg * rawEggWeightGM);
+    short rawEggWeight = 70;
+    int breakfastWeightGm = (banana * bananaWeight) + (milk100Ml * milk100MlWeight) + (iceCream * iceCreamWeight)
+            + (rawEgg * rawEggWeight);
     float breakfastWeightKg = breakfastWeightGm / 1000f;
     System.out.println("Вес завтрака: " + breakfastWeightKg + " кг");
 
@@ -45,18 +45,18 @@ public class Main {
     System.out.println("Дней для похудения при потере 500гм в день: " + daysWithDietMax);
     System.out.println("Дней для похудения в среднем: " + daysWithDietAvarage);
 
-    int MashaSalaryMonth = 67_760;
-    float MashaSalaryGrow = MashaSalaryMonth * 1.1f;
-    float MashaYearSalaryDifference = (MashaSalaryGrow * 12) % (MashaSalaryMonth * 12);
-    System.out.println("Зарплата Маши с повышением: " + MashaSalaryGrow + "; Разница в годовом доходе: " + MashaYearSalaryDifference);
-    int DenisSalaryMonth = 83_690;
-    float DenisSalaryGrow = DenisSalaryMonth * 1.1f;
-    float DenisYearSalaryDifference = (DenisSalaryGrow * 12) % (DenisSalaryMonth * 12);
-    System.out.println("Зарплата Дениса с повышением: " + DenisSalaryGrow + "; Разница в годовом доходе: " + DenisYearSalaryDifference);
-    int KristinaSalaryMonth = 76_230;
-    float KristinaSalaryGrow = KristinaSalaryMonth * 1.1f;
-    float KristinaYearSalaryDifference = (KristinaSalaryGrow * 12) % (KristinaSalaryMonth * 12);
-    System.out.println("Зарплата Кристины с повышением: " + KristinaSalaryGrow + "; Разница в годовом доходе: " + KristinaYearSalaryDifference);
+    int mashaSalaryMonth = 67_760;
+    float mashaSalaryGrow = mashaSalaryMonth * 1.1f;
+    float mashaYearSalaryDifference = (mashaSalaryGrow - mashaSalaryMonth)*12;
+    System.out.println("Зарплата Маши с повышением: " + mashaSalaryGrow + "; Разница в годовом доходе: " + mashaYearSalaryDifference);
+    int denisSalaryMonth = 83_690;
+    float denisSalaryGrow = denisSalaryMonth * 1.1f;
+    float denisYearSalaryDifference = (denisSalaryGrow - denisSalaryMonth)*12;
+    System.out.println("Зарплата Дениса с повышением: " + denisSalaryGrow + "; Разница в годовом доходе: " + denisYearSalaryDifference);
+    int kristinaSalaryMonth = 76_230;
+    float kristinaSalaryGrow = kristinaSalaryMonth * 1.1f;
+    float kristinaYearSalaryDifference = (kristinaSalaryGrow - kristinaSalaryMonth)*12;
+    System.out.println("Зарплата Кристины с повышением: " + kristinaSalaryGrow + "; Разница в годовом доходе: " + kristinaYearSalaryDifference);
 
 ;
 
